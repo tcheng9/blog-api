384b7fb6197758df202df0899ea19d73101ecf4b
Tommy C <tcheng789@gmail.com>
2022-10-16 23:50:23 -0500

    finish crud for post, now need to do comments

--- blog-api/routes/postRouter.js
+++ blog-api/routes/postRouter.js
@@ -13,8 +13,8 @@ router.get('/', async function(req, res, next){
     }
 });
 
-router.get('/:id', function (req, res,) {
-    res.send(req.params.id);
+router.get('/:id', getPost, function (req, res,) {
+    res.send(res.post.title);
 });
 
 router.post('/', async function(req, res){
@@ -35,12 +35,54 @@ router.post('/', async function(req, res){
         res.status(401).json({messsage: err.message});
      }
 })
-router.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
+router.patch('/:id', getPost, async (req, res) => {
+    if(req.body.title != null){
+        res.post.title = req.body.title
+    }
+
+    if(req.body.text != null){
+        res.post.text = req.body.text
+    }
+
+    if(req.body.comments != null){
+        res.post.comments = req.body.comments
+    }
+
+    if(req.body.publish_status != null){
+        res.post.publish_status = req.body.publish_status
+    }
+
+    try {
+        const updatedPost = await res.post.save();
+        res.json(updatedPost);
+    } catch(err){
+        res.status(400).json({message: err.message});
+    }
 });
 
-router.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
+router.delete('/:id', getPost, async (req, res) => {
+    try{
+        await res.post.remove();
+        res.json({message: "Deleted Post"});
+    } catch (err){
+        res.status(500).json({message: err.message});
+    }
 })
+
+async function getPost(req, res, next){
+    let post
+
+    try{
+        post = await Post.findById(req.params.id);
+        if (post == null){
+            return res.status(404).json({message: "Cannot find subscriber"});
+        }
+    } catch (err){
+        return res.status(500).json({message: err.message});
+    };
+
+    res.post = post
+    next();
+}
 
 module.exports = router;
