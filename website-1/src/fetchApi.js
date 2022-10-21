11cd8ec9c11041d121ca2d7bb1beeea011d7195c
tcheng9 <tcheng789@gmail.com>
2022-10-21 17:16:06 -0500

    got website 1 working - it gets all comments

--- website-1/src/fetchApi.js
+++ website-1/src/fetchApi.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 function FetchApi(){
+    const [data, setData] = useState([]);
 
     const apiGet = () => {
         fetch('http://localhost:3000/comment')
         .then((response)  => response.json())
-        .then((json) => console.log(json));
-    }
+        .then((json) => {
+            console.log(json);
+            setData(json);
+        
+    })} ;
+    
+
+    useEffect(() => {
+        apiGet();
+    }, [])
 
     return (
         <div>
@@ -15,9 +25,20 @@ function FetchApi(){
 
                 <button onClick = {apiGet}> Fetch API </button>
             </p>
+
+            <div>
+                <ul>
+                    {data.map(item => {
+                        return <li> {item.text}, {item.email} </li>
+                        
+                        
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
+
 
 
 export default FetchApi
