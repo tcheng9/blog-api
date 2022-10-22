bc7dda401f5b54cb0594fea004c5354f294c9724
Tommy C <tcheng789@gmail.com>
2022-10-22 16:57:56 -0500

    updatefor website-author page

--- website-author/src/App.js
+++ website-author/src/App.js
@@ -1,11 +1,57 @@
 
 
 function App() {
+    const 
+  function signUp(){
+    fetch("POST http://localhost:3000/auth/signup")
+      .then((response) => response.json())
+      .then((json) => {
+        
+      }
+  }
+
+  function login(){
+    console.log('login button pressed');
+  }
+
   return (
-    <div className="App">
-        test
+     <div>
+        <h1> Sign Up </h1>
+        <form id = "signup">
+          <label htmlFor = "username"> Username: </label> <br/>
+          <input type = "text"  id = "username" name = "username"/> <br/>
+
+          <label htmlFor = "password"> Password: </label> <br/>
+          <input type = "text" id = "password" name = "password"/> <br/>
+
+          <button onClick = {signUp()}> Sign Up </button> 
+        </form>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
+
+
+        <form id = "login">
+          <h1> Login Form </h1>
+          <label htmlFor = "username"> Username: </label> <br/>
+          <input type = "text"  id = "username" name = "username"/> <br/>
+
+          <label htmlFor = "password"> Password: </label> <br/>
+          <input type = "text" id = "password" name = "password"/> <br/>
+
+          <button onClick = {login()}> Log Up </button> 
+        </form>
+
+
     </div>
-  );
+  )
+ 
 }
 
 export default App;
---
+++ website-author/src/auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Auth() {
+    return (
+        <div>
+            Testtest
+        </div>
+    )
+}
+
+
+export default Auth
