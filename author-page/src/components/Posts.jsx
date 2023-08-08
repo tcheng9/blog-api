87350c6a6754a771c6656392241226b351431ce5
tcheng9 <tcheng789@gmail.com>
2023-08-07 19:47:39 -0500

    update

---
+++ author-page/src/components/Posts.jsx
@@ -0,0 +1,20 @@
+// import React, {useEffect, useState} from 'react';
+
+// const Posts = ({postId}) => {
+//     return (
+//         <div>
+//             Test Posts page
+//         </div>
+//     )
+// }
+
+function Posts(){
+    return (
+        <>
+            <p> test </p>
+            <Link to = "postsTests" > click tasdasdhis link ver. 2</Link>
+        </>
+    );
+}
+
+export default Posts;
