f7d60d4cf1a0df22789e08942c48eb2784a1fc35
tommy <tcheng789@gmail.com>
2023-08-16 13:47:48 -0500

    got skeleton new post page working

--- author-page/src/components/PostForm.jsx
+++ author-page/src/components/PostForm.jsx
@@ -3,27 +3,73 @@ import {Link} from  'react-router-dom';
 
 function PostForm(){
     const [title, setTitle] = useState('');
-    const [content, setcontent] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(title);
+        console.log(content);
+        postCreation();
+        setTitle('');
+        setContent('');
+    }
+
+    const postCreation = () => {
+        console.log('placeholder for now -- u clicked the submit button')
+        let token = localStorage.getItem('accessToken');
+        fetch('http://localhost:3000/post', {
+            method: "POST",
+            body: JSON.stringify({
+                'title': title,
+                'text': content,
+                'comments': 'placeholder',
+                'publish_status': false
+            }),
+            headers: {
+                'Content-Type':'application/json',
+                'Authorization': "Bearer " + token
+
+            }
+        })
+    }
+
     return (
         <div>
             Form to create a new post
+            <br/>
             <form>
                 <label>
                     Title
-                    <input type = "text">
+                    <input 
+                        type = "text"
+                        placeholder = "Title of post"
+                        onChange = {(e) => {
+                            setTitle(e.target.value);
+                        }}    
+                    />
                         
-                    </input>
+                    
 
                 </label>
-                
+                <br/>
 
                 <label>
                     Content
-                    <input type = "text">
+                    <input 
+                        type = "text"
+                        placeholder = "Content body"
+                        onChange = {(e) => {
+                            setContent(e.target.value);
+                        }}                        
+                    />
 
-                    </input>
+
 
                 </label>
+
+                <button onClick = {handleSubmit}>
+                    Submit post
+                </button>
             </form>
         </div>
     )
