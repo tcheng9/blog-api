6f3e80e7279cf51c733d749e8c07aac2e7c7acb4
tommy <tcheng789@gmail.com>
2023-08-17 13:45:29 -0500

    update

--- blog-api/models/post.js
+++ blog-api/models/post.js
@@ -5,8 +5,9 @@ const PostSchema = new Schema(
     {
         title: {type: String},
         text: {type: String}, 
-        comments: {type: String},
+        comments: {type: String},//placeholder for now, not sure if needed
         // comments: {type: Schema.Types.ObjectId, ref: "comment", required: true},
+        // comments: [{type: Schema.Types.ObjectId, ref: "comment"}],
         // timestamp: Date.now(),
         publish_status: {type: Boolean}
     }
