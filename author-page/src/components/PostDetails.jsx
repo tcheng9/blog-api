cdcc49518afc5ffd3c7d5de41b5b101eddcade57
tommy <tcheng789@gmail.com>
2023-08-24 22:07:19 -0500

    workingo n promise.all function still

--- author-page/src/components/PostDetails.jsx
+++ author-page/src/components/PostDetails.jsx
@@ -8,7 +8,6 @@ function PostDetails(){
     const params = useParams();
     let postId = params.id;
     let token = localStorage.getItem('accessToken');
-
     let getPostFetch = fetch(`http://localhost:3000/post/${postId}`, {
         method: 'GET',
         headers: {
@@ -22,41 +21,85 @@ function PostDetails(){
             'Authorization': 'Bearer ' + token
         }
     });
+    
 
+    //trying to call each fetch independent of each other
     useEffect(() => {
-        let process = (prom) => {
-            prom.then(data => {
-                testArr.push(data)               
-            })
-        }
-
-        Promise.all([getPostFetch, getCommentsFetch])
-        .then(allResponses => {
-            allResponses.forEach(file => {
-                process(file.json())
-            })
-            // let postFetchRes = testArr[0]
-            // let commentsFetchRes = testArr[1]
-            
-            // let postFetchRes2 = postFetchRes['post']
-
-            // setPostResults(postFetchRes2)
-            // // setCommentsResults(commentsFetchRes['comments']);
-
-            setPostResults(testArr);
-            console.log(testArr);
+        fetch(`http://localhost:3000/post/${postId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
         })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setPostResults(data);
+        })
+
+        let getCommentsFetch = fetch(`http://localhost:3000/comment/match/${postId}`, {
+            method:'GET',
+            headers:{
+                'Authorization': 'Bearer ' + token
+            }
+         })
+         .then(res => {
+            return res.json();
+         })
+         .then(data => {
+            setCommentsResults(data['comments']);
+            console.log(commentsResults)
+          
+         })
+
+    }, [])
+
+    //promise.all attempt
+    // useEffect(() => {
+
+        
+
+    //     let process = (prom) => {
+    //         prom.then(data => {
+    //             testArr.push(data)               
+    //         })
+    //     }
+
+    //     Promise.all([getPostFetch, getCommentsFetch])
+    //     .then(allResponses => {
+    //         allResponses.forEach(file => {
+    //             process(file.json())
+    //         })
+    //         return testArr
+    //     }).then( data => {
+    //         setPostResults(data);
+    //         console.log(postResults)
+    //         // let postFetchRes = data[0]
+    //         // let commentsFetchRes = data[1]
+            
+    //         // // let postFetchRes2 = postFetchRes['post']
+
+    //         // setPostResults(postFetchRes)
+    //         // setCommentsResults(commentsFetchRes);
+
+    //         // console.log(postResults);
+    //         // console.log(commentsResults);
+    //     })
+    
+            
+    //     }, [])
         
 
        
-    }, [])
+        
     
 
     return (
         <div>
             post details test page
+            {/* <p> {commentsResults[0].email} </p> */}
             
-            {/* <p> {postResults._id} </p> */}
             <p> The param id is {params.id}</p>
                  
         </div>
