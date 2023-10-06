853cfabb421afae40d31b70bd20b5a34847a459d
tcheng9 <tcheng789@gmail.com>
2023-10-06 11:28:51 -0500

    udpate

--- author-page/vite.config.js
+++ author-page/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/blog-api/'
+  base: '/blog-api/',
+  plugins: [react()]
+  
 })
